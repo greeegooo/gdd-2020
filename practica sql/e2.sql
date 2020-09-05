39545fdcd41dada34032de0bdb024ef673f57261
gmichalopulos <gregomicha@gmail.com>
2020-09-05 18:40:12 -0300

    no me acuerdo que cambie

--- practica sql/e2.sql
+++ practica sql/e2.sql
@@ -1,13 +1,11 @@
 select 
-	item.item_producto,
-	producto.prod_detalle,
-	SUM(item.item_cantidad) as cantidad
-	--factura.fact_fecha
+	producto.prod_codigo,
+	producto.prod_detalle
 from Factura as factura
-inner join Item_Factura as item on factura.fact_numero = item.item_numero
+inner join Item_Factura as item on factura.fact_numero = item.item_numero and factura.fact_tipo = item.item_tipo and factura.fact_sucursal = item.item_sucursal
 inner join Producto as producto on producto.prod_codigo = item.item_producto
 where factura.fact_fecha between '01-01-2012' and '12-31-2012'
 group by 
 	producto.prod_detalle,
-	item.item_producto
-order by cantidad desc
+	producto.prod_codigo
+order by SUM(item.item_cantidad) desc
