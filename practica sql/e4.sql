059016a8f1a1fdedd1cb1e5bede84e3571e2d5c4
Alexander <alexander.armua@mercadolibre.com>
2020-09-19 21:35:30 -0300

    Corrección 2da opción

--- practica sql/e4.sql
+++ practica sql/e4.sql
@@ -1,3 +1,4 @@
+-- 1ra solución
 select
 	prod_codigo as Codigo,
 	prod_detalle as Detalle,
@@ -14,14 +15,17 @@ group by prod_codigo, prod_detalle;
 		   
 -- 2da solución
 select 
-	p.prod_codigo as Codigo,
-	p.prod_detalle as Nombre,
-	isnull((
-		select 
-			SUM(stoc_cantidad) as Cantidad
-		FROM STOCK 
-		where stoc_producto = p.prod_codigo
-		group by stoc_producto
-	), 0) as 'Stock Total'
+	p.prod_codigo,
+	p.prod_detalle,
+	SUM(isnull(c.comp_cantidad, 0)) as ArticulosComponen
 from Producto as p
-order by Nombre asc;
+left join Composicion as c on c.comp_producto = p.prod_codigo
+right join (
+	select 
+		s.stoc_producto
+	from STOCK as s
+	group by s.stoc_producto
+		having AVG(s.stoc_cantidad) > 100
+) as stockProducto on stockProducto.stoc_producto = p.prod_codigo
+group by p.prod_codigo, p.prod_detalle
+order by ArticulosComponen desc;
