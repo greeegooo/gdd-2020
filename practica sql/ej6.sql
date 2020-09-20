878b62c213c0a921519a03aa83f2d86804a71f35
Alexander <alexander.armua@mercadolibre.com>
2020-09-20 05:46:23 -0300

    Agrego ejercicio 6

---
+++ practica sql/ej6.sql
@@ -0,0 +1,16 @@
+select 
+	p.prod_rubro as [Codigo Rubro],
+	r.rubr_detalle as [Detalle Rubro],
+	COUNT(1) AS [Cantidad Articulos],
+	MAX(suma_stock.[Stock Cantidad]) as [Stock Total]
+from Producto as p
+	join Rubro as r on r.rubr_id = p.prod_rubro
+	join (
+		select 
+			SUM(s.stoc_cantidad) as [Stock Cantidad],
+			s.stoc_producto as [Codigo Producto]
+		from STOCK as s
+		group by s.stoc_producto
+		having SUM(s.stoc_cantidad) > (select s2.stoc_cantidad from STOCK as s2 where s2.stoc_producto = '00000000' and s2.stoc_deposito = '00')
+	) as suma_stock on suma_stock.[Codigo Producto] = p.prod_codigo 
+group by p.prod_rubro, r.rubr_detalle;
