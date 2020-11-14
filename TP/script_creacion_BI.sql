7f02ab18193e3f231a853ef35c81969f5b56f7d0
agustin yañez <ayanes@est.frba.utn.edu.ar>
2020-11-14 17:40:24 -0300

    subo tablas caja motro transmision y fabricante

--- TP/script_creacion_BI.sql
+++ TP/script_creacion_BI.sql
@@ -1,6 +1,8 @@
-RAISERROR ('X - Creando tablas BI', 0, 1) WITH NOWAIT
+RAISERROR ('1 - Creando tablas BI', 0, 1) WITH NOWAIT
 GO
 
+RAISERROR ('1. - Creando [BI_Tiempo]', 0, 1) WITH NOWAIT
+GO
 -- Creación tabla Tiempo
 CREATE TABLE [LOS_CUATRO_FANTASTICOS].[BI_Tiempo] (
 	Id INT PRIMARY KEY IDENTITY(1,1),
@@ -8,11 +10,100 @@ CREATE TABLE [LOS_CUATRO_FANTASTICOS].[BI_Tiempo] (
 	Mes INT NULL
 )
 
+RAISERROR ('1. - Creando [BI_CategoriaAutoParte]', 0, 1) WITH NOWAIT
+GO
 -- Creación tabla CategoriaAutoParte
 CREATE TABLE [LOS_CUATRO_FANTASTICOS].[BI_CategoriaAutoParte] (
 	Codigo INT PRIMARY KEY IDENTITY(1,1),
 	Descripcion NVARCHAR(255) NULL
 )
 
+
+RAISERROR ('1. - Creando [BI_Fabricante]', 0, 1) WITH NOWAIT
+GO
+--Creación tabla Fabricante
+CREATE TABLE [LOS_CUATRO_FANTASTICOS].[BI_Fabricante] (
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	Nombre NVARCHAR(255) NULL,
+)
+GO
+
+RAISERROR ('1. - Creando [BI_Caja]', 0, 1) WITH NOWAIT
+GO
+--Creación tabla Caja
+CREATE TABLE [LOS_CUATRO_FANTASTICOS].[BI_Caja] (
+	Codigo DECIMAL(18,0) PRIMARY KEY,
+	Descripcion NVARCHAR(255) NOT NULL,
+
+)
+GO
+
+RAISERROR ('1. - Creando [BI_Transmision]', 0, 1) WITH NOWAIT
+GO
+--Creación tabla Transmision
+CREATE TABLE [LOS_CUATRO_FANTASTICOS].[BI_Transmision] (
+	Codigo DECIMAL(18,0) PRIMARY KEY,
+	Descripcion NVARCHAR(255)  NOT NULL,
+	Cantidad_Cambios INT NULL
+)
+GO
+
+RAISERROR ('1. - Creando [BI_Motor]', 0, 1) WITH NOWAIT
+GO
+--Creación tabla Motor
+CREATE TABLE [LOS_CUATRO_FANTASTICOS].[BI_Motor] (
+	Codigo DECIMAL(18,0) PRIMARY KEY,
+	Descripcion NVARCHAR(255)  NULL,
+)
+GO
+
 RAISERROR ('X - Fin Tablas', 0, 1) WITH NOWAIT
+GO
+
+
+
+---------------------------------------------------------
+
+---------------------------------------------------------
+
+
+RAISERROR ('2.1 - Insertando Fabricante', 0, 1) WITH NOWAIT
+GO
+-- Inserto datos de Fabricante
+INSERT INTO [LOS_CUATRO_FANTASTICOS].[BI_Fabricante] (Nombre)
+	SELECT 
+		[FABRICANTE_NOMBRE]
+	FROM gd_esquema.Maestra 
+	GROUP BY [FABRICANTE_NOMBRE]
+	ORDER BY [FABRICANTE_NOMBRE] 
+GO
+
+RAISERROR ('2.2 - Insertando Transmision', 0, 1) WITH NOWAIT
+GO
+---Inserto datos de Tipo Transmision
+INSERT INTO [LOS_CUATRO_FANTASTICOS].[BI_Transmision]([Codigo] ,[Descripcion])
+	SELECT DISTINCT [TIPO_TRANSMISION_CODIGO], [TIPO_TRANSMISION_DESC]
+	FROM [GD2C2020].[gd_esquema].[Maestra]
+	WHERE [TIPO_TRANSMISION_CODIGO] IS NOT NULL
+
+GO
+
+RAISERROR ('2.3 - Insertando Caja', 0, 1) WITH NOWAIT
+GO
+---Inserto datos de Tipo Caja
+INSERT INTO [LOS_CUATRO_FANTASTICOS].[BI_Caja]([Codigo] ,[Descripcion])
+	SELECT DISTINCT TIPO_CAJA_CODIGO, TIPO_CAJA_DESC, NULL
+	FROM [GD2C2020].[gd_esquema].[Maestra]
+	WHERE TIPO_CAJA_CODIGO IS NOT NULL 
+
+GO
+
+RAISERROR ('2.4 - Insertando Motor', 0, 1) WITH NOWAIT
+GO
+---Inserto datos de Tipo Motor
+INSERT INTO [LOS_CUATRO_FANTASTICOS].[BI_Motor]([Codigo] ,[Descripcion])
+	SELECT DISTINCT TIPO_MOTOR_CODIGO, NULL
+	FROM [GD2C2020].[gd_esquema].[Maestra]
+	WHERE TIPO_MOTOR_CODIGO IS NOT NULL
+
 GO
